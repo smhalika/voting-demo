1c536d7c08fb972e2bde37dd39908bf3bd134266
smhalika <smhalika@syr.edu>
2018-04-01 14:06:38 -0400

    changing the voting options

--- voting/app.py
+++ voting/app.py
@@ -9,8 +9,8 @@ import logging
 import signal
 import sys
 
-optionA = "Dev"
-optionB = "Ops"
+optionA = "Fotball"
+optionB = "soccer"
 name = optionA + " VS " + optionB
 
 redis = Redis(host="redis", db=0)
